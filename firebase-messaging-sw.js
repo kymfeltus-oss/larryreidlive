8f8d4b6ecdc687c847e94f7dd96cda0186d95e5e
kymfeltus-oss <kymfeltus@gmail.com>
2025-11-07 10:26:15 -0600

    Rename firebase-messaging-sw.js to public/firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++
@@ -1,1 +0,0 @@
-// Placeholder service worker for FCM web push. Configure VAPID and messaging to enable.
---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,1 @@
+// Placeholder service worker for FCM web push. Configure VAPID and messaging to enable.

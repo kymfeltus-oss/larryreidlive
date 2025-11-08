49a02eeaaf7967a7e091c4ffa3b9820180f29220
kymfeltus-oss <kymfeltus@gmail.com>
2025-11-07 19:54:43 -0600

    Create layout.js

---
+++ components/layout.js
@@ -0,0 +1,14 @@
+// components/Layout.js
+import Navbar from "./Navbar";
+import BottomTabs from "./BottomTabs";
+import "../styles/layout.css";
+
+export default function Layout({ children }) {
+  return (
+    <div className="app-shell">
+      <Navbar />
+      <main className="scroll-area">{children}</main>
+      <BottomTabs />
+    </div>
+  );
+}

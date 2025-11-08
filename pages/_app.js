0c4ee0fb19c4b85e11390dfdedd538c287870dc8
kymfeltus-oss <kymfeltus@gmail.com>
2025-11-07 20:48:07 -0600

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import "../styles/globals.css";
 import "../styles/layout.css";
 import "../styles/home.css";
 import "../styles/memberhub.css";
+import "../styles/membership.css";
 
 import Layout from "../components/Layout";
 

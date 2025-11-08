50521fc07eed68b41437311070500d7c77a46a36
kymfeltus-oss <kymfeltus@gmail.com>
2025-11-07 20:14:27 -0600

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,7 +1,11 @@
 export default function Navbar() {
   return (
     <header className="app-navbar">
-      <img src="/assets/logo.png" alt="LRL" className="app-logo" />
+      <img
+        src="/assets/logo.png"
+        alt="Larry Reid Live"
+        className="app-logo"
+      />
     </header>
   );
 }

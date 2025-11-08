bf51e858ff69380578f5fabfc1100c8c4de84e01
kymfeltus-oss <kymfeltus@gmail.com>
2025-11-07 20:06:43 -0600

    Create valut.js

---
+++ pages/valut.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+export default function Vault() {
+  return (
+    <>
+      <Head><title>The Vault — LRL</title></Head>
+      <section className="mock-page">
+        <h1>The Vault</h1>
+        <p>Archived teachings and media for all LRL’s will appear here soon.</p>
+      </section>
+    </>
+  );
+}

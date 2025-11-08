1a0800bbd04fc3f36ff0b7436449723533dfc94e
kymfeltus-oss <kymfeltus@gmail.com>
2025-11-08 00:37:37 -0600

    Update video-test.js

--- pages/video-test.js
+++ pages/video-test.js
@@ -10,3 +10,15 @@ export default function VideoTest() {
     />
   );
 }
+export default function VideoTest() {
+  return (
+    <video
+      autoPlay
+      loop
+      muted
+      playsInline
+      style={{ width: "100%", height: "100vh", objectFit: "cover" }}
+      src="/assets/hero-bg.mp4"
+    />
+  );
+}
